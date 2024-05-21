101e2067559ebed45c5a8e9403b3a806aac3910b
Bright Tetena <zbtetena@gmail.com>
2024-05-22 01:21:33 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+var icon = document.getElementById("icon");
+const body = document.body;
+console.log(body);
+
+icon.addEventListener("click", function (event) {
+  event.preventDefault();
+  body.classList.toggle("dark");
+});
+var ham = document.getElementById("ham");
+const nav = document.getElementById("nav");
+
+ham.onclick = function (event) {
+  body.classList.toggle("active");
+};
